05fbc9cccf9f1821d1198691f81f6ff2e26fddd3
Ahmed Nuaman <ahmed@ahmednuaman.com>
2017-06-13 13:51:54 +0100

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 0212VF-qliksense-deploy
+
+## This is to be used with [qliksense-template](https://github.com/ahmednuaman/qliksense-template)!
+
+## How to use
+`HOST=http://qliksense-host/static_user/qrs/ USERID=QRS-USER MASHUP=yes qliksense-deploy`
+
+Environment variable `MASHUP` is optional, but if it is defined then you need to ensure your `package.json` includes:
+
+```
+"deploy": {
+  "id": "mashup-unique-id", // usually same as package.json's name
+  "cdn": "mashup_unique_id_content" // same as id except snake case with _content at the end
+}
+```
---
+++ index.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+require('colors')
+
+const async = require('async')
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
+const request = require('request')
+
+const BUILD_DIR = './build'
+const CONTENT_DIR = 'content'
+const CWD = process.cwd()
+const HOST = process.env.HOST
+const MASHUP = !!process.env.MASHUP
+const PKG = require(path.resolve(CWD, './package.json'))
+const XRF_KEY = require('randomstring').generate({
+  length: 16,
+  charset: 'alphanumeric'
+})
+const ZIP_FILE = `${PKG.name}.zip`
+
+let requests = []
+
+const headers = {
+  'X-Qlik-Xrfkey': XRF_KEY,
+  userid: process.env.USERID
+}
+const qs = {
+  xrfkey: XRF_KEY
+}
+
+const extension = request.defaults({
+  baseUrl: `${HOST}extension/`,
+  headers,
+  qs
+})
+
+requests.push((done) => {
+  extension.delete(`name/${PKG.name}`, (error) => {
+    console.log(`Successfully deleted old extension ${PKG.name}`)
+    done(error)
+  })
+})
+
+requests.push((done) => {
+  const stream = fs.createReadStream(path.join(CWD, BUILD_DIR, ZIP_FILE))
+
+  stream.on('error', done)
+  stream.pipe(
+    extension
+      .post('upload', {
+        headers: Object.assign(headers, {
+          'content-type': 'application/x-www-form-urlencoded'
+        })
+      })
+      .on('error', (error) => done(error.toString()))
+      .on('data', (data) => console.log(PKG.name, data.toString()))
+      .on('response', (response) => {
+        console.log(`Successfully deployed extension ${PKG.name}`, response.statusCode)
+        done()
+      })
+  )
+})
+
+if (MASHUP) {
+  if (!PKG.deploy || !PKG.deploy.id || !PKG.deploy.cdn) {
+    throw new Error('Missing deploy: { id, cdn } config in package.json')
+  }
+
+  const content = request.defaults({
+    baseUrl: `${HOST}contentlibrary/${PKG.deploy.id}/uploadfile`,
+    headers
+  })
+
+  const files = glob.sync(path.join(CWD, BUILD_DIR, CONTENT_DIR, PKG.deploy.id, PKG.deploy.cdn, '*'))
+
+  requests = requests.concat(
+    files
+      .map((file) => (done) => {
+        const externalpath = path.join(PKG.deploy.cdn, path.basename(file))
+        const stream = fs.createReadStream(file)
+
+        stream.on('error', done)
+        stream.pipe(
+          content
+            .post('', {
+              qs: Object.assign(qs, {
+                overwrite: true,
+                externalpath
+              })
+            })
+            .on('error', (error) => done(error.toString()))
+            .on('data', (data) => console.log(`Successfully deployed shared content ${externalpath} to ${data.toString()}`))
+            .on('response', (response) => done())
+        )
+      })
+  )
+}
+
+async.series(requests, (error) => {
+  if (error) {
+    console.log(error.red)
+  } else {
+    console.log('All files successfully deployed'.bold.green)
+  }
+})
